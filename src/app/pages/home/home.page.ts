6288650c00d11523331c88c0d231287544be900e
Souradeep <ghoshsouradeep475@gmail.com>
2022-01-29 00:48:14 +0530

    change

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -89,6 +89,7 @@ export class HomePage implements OnInit {
     let upcomingdutys = [];
     let olddutys = [];
     data.forEach(element => {
+      element.phone = "tel:" + element.reporttonum;
       if(new Date(element.dutydate) >= new Date()){
         if(this.dutyid){
           if(element.dutyid === this.dutyid){
