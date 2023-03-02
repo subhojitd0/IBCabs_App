80b9269e38ffb629512cfcd5cced0ddc30ed12b6
Souradeep <ghoshsouradeep475@gmail.com>
2023-03-02 23:40:57 +0530

    sign changes

--- src/app/shared/services/service.ts
+++ src/app/shared/services/service.ts
@@ -56,7 +56,7 @@ export class ApiService {
   public post(url: any, jsonData: any){
     return fetch(API_URL + url, {
         method: "POST",
-        headers: { "Content-Type" : "application/json" },
+        //headers: { "Content-Type" : "application/json" },
         body: JSON.stringify(jsonData)
     }).then(response => response.json());
   }
